519c5bc378743a36f7c143001cae0e5828d7733d
Brian Hill <76450501+bhillkeyfactor@users.noreply.github.com>
2024-04-24 12:26:58 -0400

    Dev release 2.1 (#14)
    * Support for Pan Level Certificates
    * Support for Pushing Entire Certificate Chain to Panorama
    * Auto Detection of Trusted Root Certificates
    * Fix Inventory Check For Private Key from Dummy to Anything

--- PaloAltoTestConsole/KeyfactorClient.cs
+++ PaloAltoTestConsole/KeyfactorClient.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright 2023 Keyfactor
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +25,15 @@ namespace PaloAltoTestConsole
     {
         public async Task<KeyfactorEnrollmentResult> EnrollCertificate(string commonName)
         {
-            var options = new RestClientOptions("https://URLToKeyfactor");
+            var options = new RestClientOptions("https://kfcommandurl.com");
             var client = new RestClient(options);
             var request = new RestRequest("/KeyfactorAPI/Enrollment/PFX", Method.Post);
             request.AddHeader("X-Keyfactor-Requested-With", "APIClient");
             request.AddHeader("x-certificateformat", "PFX");
-            request.AddHeader("Authorization", "Basic BasicAuthKey");
+            request.AddHeader("Authorization", "Basic fsadfsdafds=");
             request.AddHeader("Content-Type", "application/json");
             var enrollRequest = new KeyfactorEnrollmentRequest
             {
-                CustomFriendlyName = "2 Year Web Server",
                 Password = "sldfklsdfsldjfk",
                 PopulateMissingValuesFromAD = false,
                 Subject = $"CN={commonName}",
@@ -35,7 +48,7 @@ namespace PaloAltoTestConsole
             sans.DNS = dnsList;
             enrollRequest.SANs = sans;
             request.AddBody(enrollRequest);
-            var response = await client.ExecuteAsync<KeyfactorEnrollmentResult>(request);
+            var response = await client.ExecutePostAsync<KeyfactorEnrollmentResult>(request);
             return response.Data;
 
         }
