2ff520d54f59d771d91c5a4602b8e08e3743931b
Brian Hill <bhill@keyfactor.com>
2024-10-29 14:44:36 +0000

    Make Trusted Cert Inventory Optional to save time, most users I have talked to don't care about inventoring these and they take a ton of time.

--- PaloAlto/JobProperties.cs
+++ PaloAlto/JobProperties.cs
@@ -23,6 +23,8 @@ namespace Keyfactor.Extensions.Orchestrator.PaloAlto
         [DefaultValue("")]
         public string DeviceGroup { get; set; }
 
-
+        [JsonProperty("InventoryTrustedCerts")]
+        [DefaultValue(false)]
+        public bool InventoryTrustedCerts { get; set; }
     }
 }
